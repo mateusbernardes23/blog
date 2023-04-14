c9e88a872a8a1b412f894a0daca601a0f61e812c
mateusbernardes23 <mateusbernardes23@hotmail.com>
2023-04-14 19:57:40 -0300

    Create connection in database

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso!");
+    }).catch((error) => {
+        console.log(error);
+    })
 
 app.get("", (red, res) => {
-    res.send("Bem vindo ao site");
+    res.render("index");
 });
 
 app.listen(3000, () => {
