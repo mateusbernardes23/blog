405af280973514a38bd81e70287033661bd7b9b7
mateusbernardes <mateusbernardes23@hotmail.com>
2023-06-14 20:57:30 -0300

    Criado home page

--- index.js
+++ index.js
@@ -28,9 +28,36 @@ app.use("/", categoriesController);
 app.use("/", articlesController);
     
 
-app.get("", (red, res) => {
-    res.render("index");
+app.get("/", (req, res) => {
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {categories: categories, articles: articles});
+        });
+    });
 });
+
+app.get("/:slug", (req, res) => {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined) {
+            Category.findAll().then(category => {
+                res.render("article", {category: category, article: article});
+            });
+        } else {
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+})
 
 app.listen(3000, () => {
     console.log("Servidor rodando!");
